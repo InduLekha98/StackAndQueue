241d2f9ab88ec9389d64224f70b72908b6ab09a4
InduLekha98 <rindulekha09@gmail.com>
2021-03-15 00:59:37 +0530

    Added:Ability to dequeue from the beginning

--- QueueTest.java
+++ QueueTest.java
@@ -1,53 +1,52 @@
-import org.junit.jupiter.api.Assertions
-
 import linkedlist.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class QueueTest {
 
-        @Test
-        public void givenValues_WhenAddedToStack_ShouldAddedToTheTopOfQueue() {
-            Node<Integer> firstNode = new Node<Integer>(70);
-            Node<Integer> secondNode = new Node<Integer>(30);
-            Node<Integer> thirdNode = new Node<Integer>(56);
-            Queue<Integer> Queue = new Queue<>();
-            Queue.push(firstNode);
-            Queue.push(secondNode);
-            Queue.push(thirdNode);
-            Queue.printStack();
-            boolean result = thirdNode.getNext().equals(secondNode) &&
-                    secondNode.getNext().equals(firstNode);
-            Assertions.assertTrue(result);
-        }
-
-        @Test
-        public void whenPeekOperationInQueue_ShouldReturnTopValueOfQueue() {
-            Node<Integer> firstNode = new Node<Integer>(70);
-            Node<Integer> secondNode = new Node<Integer>(30);
-            Node<Integer> thirdNode = new Node<Integer>(56);
-            Queue<Integer> Queue = new Queue<>();
-            Queue.push(firstNode);
-            Queue.push(secondNode);
-            Queue.push(thirdNode);
-            Queue.printQueue();
-            Node<Integer> topNode = Queue.peek();
-            System.out.println("Top of stack: " + topNode.getValue());
-            Assert.assertEquals(thirdNode, topNode);
-        }
-
-        @Test
-        public void whenPopkOperationInQueue_ShouldRemoveValueFromTopOfQueue() {
-            Node<Integer> firstNode = new Node<Integer>(70);
-            Node<Integer> secondNode = new Node<Integer>(30);
-            Node<Integer> thirdNode = new Node<Integer>(56);
-            Queue<Integer> Queue = new Queue<>();
-            Queue.push(firstNode);
-            Queue.push(secondNode);
-            Queue.push(thirdNode);
-            Queue.printQueue();
-            Node<Integer> topNode = Queue.pop();
-            System.out.println("Removed Value: " + topNode.getValue());
-            Queue.printQueue();
-            Assertions.assertEquals(thirdNode, topNode);
-        }
+    @Test
+    public void givenValues_WhenAddedToQueue_ShouldAddedToTheTopOfQueue() {
+        Node<Integer> firstNode = new Node<Integer>(70);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(56);
+        Queue<Integer> Queye = new Queue<>();
+        Queye.push(firstNode);
+        Queye.push(secondNode);
+        Queye.push(thirdNode);
+        Queye.printQueue();
+        boolean result = thirdNode.getNext().equals(secondNode) &&
+                secondNode.getNext().equals(firstNode);
+        Assertions.assertTrue(result);
     }
+
+    @Test
+    public void whenPeekOperationInQueue_ShouldReturnTopValueOfQueue() {
+        Node<Integer> firstNode = new Node<Integer>(70);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(56);
+        Queue<Integer> Queue = new Queue<>();
+        Queue.push(firstNode);
+        Queue.push(secondNode);
+        Queue.push(thirdNode);
+        Queue.printQueue();
+        Node<Integer> topNode = Queue.peek();
+        System.out.println("Top of Queue: " + topNode.getValue());
+        Assertions.assertEquals(thirdNode, topNode);
+    }
+
+    @Test
+    public void whenPopkOperationInQueue_ShouldRemoveValueFromTopOfQueue() {
+        Node<Integer> firstNode = new Node<Integer>(70);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(56);
+        Queue<Integer> Queue = new Queue<>();
+        Queue.push(firstNode);
+        Queue.push(secondNode);
+        Queue.push(thirdNode);
+        Queue.printQueue();
+        Node<Integer> topNode = Queue.pop();
+        System.out.println("Removed Value: " + topNode.getValue());
+        Queue.printQueue();
+        Assertions.assertEquals(thirdNode, topNode);
+    }
+}
