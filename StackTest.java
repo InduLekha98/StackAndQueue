a42ae9a1b171a5df2902a5533f225bff463dad57
InduLekha98 <rindulekha09@gmail.com>
2021-03-15 00:28:20 +0530

    Added:Ability to peak and pop from the stack

--- StackTest.java
+++ StackTest.java
@@ -1,8 +1,7 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 import linkedlist.*;
-
+import org.junit.jupiter.api.Test;
 
 public class StackTest {
 
@@ -18,9 +17,20 @@ public class StackTest {
         stack.printStack();
         boolean result = thirdNode.getNext().equals(secondNode) &&
                 secondNode.getNext().equals(firstNode);
-        Assertions.assertTrue(result);
+        Assert.assertTrue(result);
     }
 
-}
-
-}
+    @Test
+    public void whenPeekOperationInStack_ShouldReturnTopValueOfStack() {
+        Node<Integer> firstNode = new Node<Integer>(70);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(56);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(firstNode);
+        stack.push(secondNode);
+        stack.push(thirdNode);
+        stack.printStack();
+        Node<Integer> topNode = stack.peek();
+        System.out.println("Top of stack: " + topNode.getValue());
+        Assertions.assertEquals(thirdNode, topNode);
+    }
